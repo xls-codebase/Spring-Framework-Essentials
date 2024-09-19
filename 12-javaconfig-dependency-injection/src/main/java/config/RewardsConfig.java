4d59ee109006ef32e185f9e14f849af87ff5da4c
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-19 20:50:42 +0200

    Java Configuration Lab TODO-04: Implement factory methods

--- 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -2,8 +2,13 @@ package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
 import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
 import rewards.internal.reward.RewardRepository;
 
 import javax.sql.DataSource;
@@ -16,25 +21,6 @@ import javax.sql.DataSource;
  * - Injecting dependencies through constructor injection
  * - Creating Spring application context in the test code
  *   (WITHOUT using Spring testContext framework)
- *
- * TODO-03: Inject DataSource through constructor injection
- * - Each repository implementation has a DataSource
- *   property to be set, but the DataSource is defined
- *   elsewhere (TestInfrastructureConfig.java), so you
- *   will need to define a constructor for this class
- *   that accepts a DataSource parameter.
- * - As it is the only constructor, @Autowired is optional.
- *
- * TODO-04: Implement each @Bean method to contain the code
- *          needed to instantiate its object and set its
- *          dependencies
- * - You can create beans from the following implementation classes
- *   - rewardNetwork bean from RewardNetworkImpl class
- *   - accountRepository bean from JdbcAccountRepository class
- *   - restaurantRepository bean from JdbcRestaurantRepository class
- *   - rewardRepository bean from JdbcRewardRepository class
- * - Note that return type of each bean method should be an interface
- *   not an implementation.
  */
 
 @Configuration
@@ -47,19 +33,29 @@ public class RewardsConfig {
     }
 
     @Bean
-    public void rewardNetwork(AccountRepository accountRepository, RestaurantRepository restaurantRepository, RewardRepository rewardRepository) {
+    public RewardNetwork rewardNetwork(AccountRepository accountRepository, RestaurantRepository restaurantRepository, RewardRepository rewardRepository) {
+        return new RewardNetworkImpl(accountRepository, restaurantRepository, rewardRepository);
     }
 
     @Bean
-    public void accountRepository(DataSource dataSource) {
+    public AccountRepository accountRepository(DataSource dataSource) {
+        JdbcAccountRepository accountRepository = new JdbcAccountRepository();
+        accountRepository.setDataSource(dataSource);
+        return accountRepository;
     }
 
     @Bean
-    public void restaurantRepository(DataSource dataSource) {
+    public RestaurantRepository restaurantRepository(DataSource dataSource) {
+        JdbcRestaurantRepository restaurantRepository = new JdbcRestaurantRepository();
+        restaurantRepository.setDataSource(dataSource);
+        return restaurantRepository;
     }
 
     @Bean
-    public void rewardRepository(DataSource dataSource) {
+    public RewardRepository rewardRepository(DataSource dataSource) {
+        JdbcRewardRepository rewardRepository = new JdbcRewardRepository();
+        rewardRepository.setDataSource(dataSource);
+        return rewardRepository;
     }
 
 }
