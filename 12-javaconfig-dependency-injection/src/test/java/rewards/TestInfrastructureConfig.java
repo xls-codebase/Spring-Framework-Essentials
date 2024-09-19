963ad4236a1a772d30312f5a7fb64ece979d6ac5
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-19 23:19:11 +0200

    Java Configuration Lab TODO-11: Run the tests

--- 12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
+++ 12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
@@ -8,24 +8,6 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
 
-/**
- * TODO-10: We can test the setup by running an empty test.
- * - If your IDE automatically generated a @Test method, rename it
- *   testRewardForDining. Delete any code in the method body.
- * - Otherwise add a testRewardForDining method & annotate it with
- *   @Test (make sure the @Test is from org.junit.jupiter.api.Test ).
- * - Run the test. If your setup() is working, you get a green bar.
- *
- * TODO-11: Finally run a real test.
- * - Copy the unit test (the @Test method) from
- *   RewardNetworkImplTests#testRewardForDining() under
- *   rewards.internal test package - we are testing
- *   the same code, but using a different setup.
- * - Run the test - it should pass if you have configured everything
- *   correctly. Congratulations, you are done.
- * - If your test fails - did you miss the import in TO DO 7 above?
- *
- */
 @Configuration
 @Import(RewardsConfig.class)
 public class TestInfrastructureConfig {
