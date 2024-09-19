fb1aeef8fe7494fdfe8c723aa3d72bf0f30b9247
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-19 21:28:54 +0200

    Java Configuration Lab TODO-05: Fix RewardsConfig and run the test

--- 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -13,16 +13,6 @@ import rewards.internal.reward.RewardRepository;
 
 import javax.sql.DataSource;
 
-/**
- * TODO-00: In this lab, you are going to exercise the following:
- * - Creating Spring configuration class
- * - Defining bean definitions within the configuration class
- * - Specifying the dependency relationships among beans
- * - Injecting dependencies through constructor injection
- * - Creating Spring application context in the test code
- *   (WITHOUT using Spring testContext framework)
- */
-
 @Configuration
 public class RewardsConfig {
 
@@ -33,26 +23,26 @@ public class RewardsConfig {
     }
 
     @Bean
-    public RewardNetwork rewardNetwork(AccountRepository accountRepository, RestaurantRepository restaurantRepository, RewardRepository rewardRepository) {
-        return new RewardNetworkImpl(accountRepository, restaurantRepository, rewardRepository);
+    public RewardNetwork rewardNetwork() {
+        return new RewardNetworkImpl(accountRepository(), restaurantRepository(), rewardRepository());
     }
 
     @Bean
-    public AccountRepository accountRepository(DataSource dataSource) {
+    public AccountRepository accountRepository() {
         JdbcAccountRepository accountRepository = new JdbcAccountRepository();
         accountRepository.setDataSource(dataSource);
         return accountRepository;
     }
 
     @Bean
-    public RestaurantRepository restaurantRepository(DataSource dataSource) {
+    public RestaurantRepository restaurantRepository() {
         JdbcRestaurantRepository restaurantRepository = new JdbcRestaurantRepository();
         restaurantRepository.setDataSource(dataSource);
         return restaurantRepository;
     }
 
     @Bean
-    public RewardRepository rewardRepository(DataSource dataSource) {
+    public RewardRepository rewardRepository() {
         JdbcRewardRepository rewardRepository = new JdbcRewardRepository();
         rewardRepository.setDataSource(dataSource);
         return rewardRepository;
