9eb5f4fdfc3f2a49db9182e875a55a0ad7133684
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-19 15:20:12 +0200

    Java Configuration Lab TODO-02: Define four empty @Bean methods, one for the reward-network and three for the repositories.

--- 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,6 +1,10 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.RewardRepository;
 
 import javax.sql.DataSource;
 
@@ -12,9 +16,6 @@ import javax.sql.DataSource;
  * - Injecting dependencies through constructor injection
  * - Creating Spring application context in the test code
  *   (WITHOUT using Spring testContext framework)
- *
- * TODO-01: Make this class a Spring configuration class
- * - Use an appropriate annotation.
  *
  * TODO-02: Define four empty @Bean methods, one for the
  *          reward-network and three for the repositories.
@@ -47,7 +48,22 @@ import javax.sql.DataSource;
 @Configuration
 public class RewardsConfig {
 
-	// Set this by adding a constructor.
 	private DataSource dataSource;
+
+    @Bean
+    public void rewardNetwork(AccountRepository accountRepository, RestaurantRepository restaurantRepository, RewardRepository rewardRepository) {
+    }
+
+    @Bean
+    public void accountRepository(DataSource dataSource) {
+    }
+
+    @Bean
+    public void restaurantRepository(DataSource dataSource) {
+    }
+
+    @Bean
+    public void rewardRepository(DataSource dataSource) {
+    }
 
 }
