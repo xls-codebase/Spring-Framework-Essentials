c88832c91dc428ea415e208e26b00bde1061fdee
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-21 18:50:57 +0200

    Refactory: remove unused code

--- 16-annotations/src/main/java/config/RewardsConfig.java
+++ 16-annotations/src/main/java/config/RewardsConfig.java
@@ -2,45 +2,9 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import rewards.RewardNetwork;
-import rewards.internal.RewardNetworkImpl;
-import rewards.internal.account.AccountRepository;
-import rewards.internal.account.JdbcAccountRepository;
-import rewards.internal.restaurant.JdbcRestaurantRepository;
-import rewards.internal.restaurant.RestaurantRepository;
-import rewards.internal.reward.JdbcRewardRepository;
-import rewards.internal.reward.RewardRepository;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("rewards.internal")
 public class RewardsConfig {
 
-	DataSource dataSource;
-		
-	public RewardNetwork rewardNetwork(){
-		return new RewardNetworkImpl(
-			accountRepository(), 
-			restaurantRepository(), 
-			rewardRepository());
-	}
-	
-	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository();
-		repository.setDataSource(dataSource);
-		return repository;
-	}
-	
-	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
-	}
-	
-	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository();
-		repository.setDataSource(dataSource);
-		return repository;
-	}
-	
 }
