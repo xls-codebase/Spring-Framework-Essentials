e92dd347226cbed16a1ca61dd8287240be0db8c8
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-24 21:48:28 +0200

    Introducing Aspect Oriented Programming Lab TODO 01: Setup Tests to enable console output

--- 22-aop/src/test/java/rewards/TestConstants.java
+++ 22-aop/src/test/java/rewards/TestConstants.java
@@ -6,9 +6,6 @@ public class TestConstants {
 	// - Creating aspect using Spring AOP
 	// - Writing pointcut expressions
 	// - Using various types of advices
-	//
-	// TODO-01: Enable checking of console output in our Tests.
-	// - Change the value below to true
 	
-	public static final boolean CHECK_CONSOLE_OUTPUT = false;
+	public static final boolean CHECK_CONSOLE_OUTPUT = true;
 }
