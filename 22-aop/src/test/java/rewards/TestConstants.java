5b92c7f268ed652b62d02a3a5bad889dae7fe00e
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-25 13:53:27 +0200

    Refactory: remove resolved TODO-Comments

--- 22-aop/src/test/java/rewards/TestConstants.java
+++ 22-aop/src/test/java/rewards/TestConstants.java
@@ -1,11 +1,6 @@
 package rewards;
 
 public class TestConstants {
-	
-	// TODO-00: In this lab, you are going to exercise the following:
-	// - Creating aspect using Spring AOP
-	// - Writing pointcut expressions
-	// - Using various types of advices
-	
+
 	public static final boolean CHECK_CONSOLE_OUTPUT = true;
 }
