ef4e1cd637fc0009429d52e4c38947112c42d834
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-25 13:32:22 +0200

    Introducing Aspect Oriented Programming Lab TODO 10: Add advice for exception handling

--- 22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
+++ 22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
@@ -1,9 +1,9 @@
 package rewards.internal.aspects;
 
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import rewards.internal.exception.RewardDataAccessException;
 
 
@@ -14,14 +14,7 @@ public class DBExceptionHandlingAspect {
 	
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-
-	//	TODO-10 (Optional): Use AOP to log an exception.
-	//  (Steps 10, 11 and 12 are optional, skip them if you are short on time)
-	//
-	//  - Configure this advice method to enable logging of
-	//	  exceptions thrown by Repository class methods.
-	//	- Select the advice type that seems most appropriate.
-	
+	@AfterThrowing(value="execution public * rewards.internal.*.*Repository.*(..))", throwing = "e")
 	public void implExceptionHandling(RewardDataAccessException e) {
 		// Log a failure warning
 		logger.warn(EMAIL_FAILURE_MSG + e + "\n");
