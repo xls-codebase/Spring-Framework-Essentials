6db7fef3134619ed0a91bbd089ad28459194e166
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-25 13:34:54 +0200

    Introducing Aspect Oriented Programming Lab TODO 11: Wire the aspect for component scan

--- 22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
+++ 22-aop/src/main/java/rewards/internal/aspects/DBExceptionHandlingAspect.java
@@ -4,10 +4,12 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import rewards.internal.exception.RewardDataAccessException;
 
 
-@Aspect	
+@Aspect
+@Component
 public class DBExceptionHandlingAspect {
 	
 	public static final String EMAIL_FAILURE_MSG = "Failed sending an email to Mister Smith : ";
@@ -19,8 +21,5 @@ public class DBExceptionHandlingAspect {
 		// Log a failure warning
 		logger.warn(EMAIL_FAILURE_MSG + e + "\n");
 	}
-
-	//	TODO-11 (Optional): Annotate this class as a Spring-managed bean.
-	//	- Note that we enabled component scanning in an earlier step.
 
 }
