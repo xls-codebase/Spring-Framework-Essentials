650cc031952774a09fcdd966fc04ca19b2951ac1
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-09-25 13:38:44 +0200

    Introducing Aspect Oriented Programming Lab TODO 12: Run aspect tests: WORKED!

--- 22-aop/src/test/java/rewards/DBExceptionHandlingAspectTests.java
+++ 22-aop/src/test/java/rewards/DBExceptionHandlingAspectTests.java
@@ -35,13 +35,6 @@ public class DBExceptionHandlingAspectTests {
             repository.findByCreditCard("1234123412341234");
         });
 
-        // TODO-12: (Optional) Validate our AOP is working.
-        //
-        // - An error message should now be logged to the console as a warning
-        // - Save all your work and run this test - it should pass with a warning
-        //   message on the console AND the console output assertion (below)
-        //   should succeed.
-
         if (TestConstants.CHECK_CONSOLE_OUTPUT) {
             assertThat(capture.toString(), containsString(DBExceptionHandlingAspect.EMAIL_FAILURE_MSG));
         }
